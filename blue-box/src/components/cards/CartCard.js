492c65ac3ebe998a6eafa232a3b359149d3936d7
RealPatrickGuertin <63614060+RealPatrickGuertin@users.noreply.github.com>
2021-04-28 09:43:36 -0400

    cart add and remove working properly

--- blue-box/src/components/cards/CartCard.js
+++ blue-box/src/components/cards/CartCard.js
@@ -1,26 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Context } from '../../databases/Store'
 import '../../styles/Card.css'
 import Users from '../../databases/userDatabase'
 
-function onRemoveFromCart(e, username) {
-    for(let i = 0; i < Users.length; i++) {
-        if(Users[i].username === username) {
-            for(let j = 0; j < Users[i].cart.length; j++) {
-                if(Users[i].cart[j] !== undefined) {
-                    if(Users[i].cart[j].id === e.id) {
-                        //update components
-                        delete Users[i].cart[j]
-                    } 
+function CartCard(props) {
+    const [state] = useContext(Context)
+    const [cart, setCart] = useState(false)
+
+    function onRemoveFromCart(e, username) {
+        for(let i = 0; i < Users.length; i++) {
+            if(Users[i].username === username) {
+                for(let j = 0; j < Users[i].cart.length; j++) {
+                    if(Users[i].cart[j] !== undefined) {
+                        if(Users[i].cart[j].id === e.id) {
+                            //update components
+                            delete Users[i].cart[j]
+                            setCart(true)
+                            console.log(cart)
+                            break
+                        }
+                    }
                 }
             }
         }
     }
-    
-}
 
-function CartCard(props) {
-    const [state] = useContext(Context)
     return (
         <div className = "card">
             <div className="container">
